67e8a409f2fbf633ee138ddd8e52928236fdb9c3
Ryu2312 <alessiasophia23@gmail.com>
2025-02-23 23:57:55 -0500

    union de los componentes y creacion de la vista

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import FilterForm from "./components/FilterForm";
+import NoteCard from "./components/NoteCard";
+import NoteForm from "./components/NoteForm";
 
 function App() {
-
+  
   return (
-    <h1>Hola mundo</h1>
-  )
+    <>
+      <div className="container mt-4 col-xxl-8">
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-5 col-md-8 col-10 "> 
+            <h1 className="mb-3 text-center">Notas App</h1>
+            <NoteForm/>
+          </div>
+        </div>
+        <div className="d-flex gap-4 flex-column flex-md-row">
+          <FilterForm/> 
+          <div className="col-md-9 mt-4">
+            <h2 className="mb-3">Notas</h2>
+            {false ? (
+              <p>No hay notas disponibles</p>
+            ) : (<NoteCard/>
+              )}
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
