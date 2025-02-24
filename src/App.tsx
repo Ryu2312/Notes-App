6371cdc329feb13e0949093e48230001ae1fd4db
Ryu2312 <alessiasophia23@gmail.com>
2025-02-24 03:00:21 -0500

    estructuracion de carpetas para granularidad

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import FilterForm from "./components/FilterForm";
-import NoteCard from "./components/NoteCard";
-import NoteForm from "./components/NoteForm";
+import Home from "./pages/Home";
 
 function App() {
   
   return (
-    <>
-      <div className="container mt-4 col-xl-8">
-        <div className="row d-flex justify-content-center">
-          <div className="col-lg-5 col-md-8 col-10 "> 
-            <h1 className="mb-3 text-center">Notas App</h1>
-            <NoteForm/>
-          </div>
-        </div>
-        <div className="d-flex gap-4 flex-column flex-md-row">
-          <FilterForm/> 
-          <div className="col-md-9 mt-4">
-            <h2 className="mb-3">Notas</h2>
-            {false ? (
-              <p>No hay notas disponibles</p>
-            ) : (<NoteCard/>
-              )}
-            
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -1,44 +1,48 @@
 function FilterForm () {
 return (
-    <div className="mt-4 ">
-          <form className="col gap-4 d-flex flex-md-column flex-row justify-content-around">
-            <div className="mb-3">
-              <label htmlFor="filterCategory" className="form-label">Ordenar por:</label>
-              <select id="filterCategory" className="form-select">
-                <option value="creacion" defaultChecked>Fecha de Creacion</option>
-                <option value="modificacion">Fecha de modificacion</option>
-                <option value="asc">Ascendente(A-Z)</option>
-                <option value="desc">Desendente(Z-A)</option>
-              </select>
-            </div>
-            <div className="mb-3">
-              <span>Filtrar por:</span>
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="radioAsc"
-                  value="pending"
-                />
-                <label className="form-check-label" htmlFor="radioAsc">
-                  Pendiente
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="radioDesc"
-                  value="important"
-                />
-                <label className="form-check-label" htmlFor="radioDesc">
-                  Importante
-                </label>
-              </div>
-            </div>
-          </form>
+  <div className="mt-4">
+    <form className="sticky-top gap-4 d-flex flex-md-column flex-row justify-content-around" style={{top: "3rem"}}>
+      <div className="mb-3">
+        <label htmlFor="filterCategory" className="form-label">
+          Ordenar por:
+        </label>
+        <select id="filterCategory" className="form-select">
+          <option value="creacion" defaultChecked>
+            Fecha de Creacion
+          </option>
+          <option value="modificacion">Fecha de modificacion</option>
+          <option value="asc">Ascendente(A-Z)</option>
+          <option value="desc">Desendente(Z-A)</option>
+        </select>
+      </div>
+      <div className="mb-3">
+        <span>Filtrar por:</span>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="radioAsc"
+            value="pending"
+          />
+          <label className="form-check-label" htmlFor="radioAsc">
+            Pendiente
+          </label>
         </div>
-)
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="radioDesc"
+            value="important"
+          />
+          <label className="form-check-label" htmlFor="radioDesc">
+            Importante
+          </label>
+        </div>
+      </div>
+    </form>
+  </div>
+);
 }
 
 export default FilterForm
